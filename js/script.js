d9b96064fdca1ce3666ae17acba1926d4cc935fd
ryoya.working@gmail.com <ryoya.working@gmail.com>
2022-03-14 10:01:28 +0900

    レスポンシブ対応

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 var mySwiper = new Swiper('.swiper', {
-    slidesPerView: 1.2,
+    slidesPerView: 1,
     initialSlide: 0,
     spaceBetween: 16,
-    centeredSlides : false,
+    centeredSlides: true,
     loop: true,
     speed:1500,
     clickable: true,
@@ -16,18 +16,59 @@ var mySwiper = new Swiper('.swiper', {
         clickable: true,
     },
     breakpoints:{
-        510:{
+        600:{
             slidesPerView: 2,
-            centeredSlides: true,
             slidesPerGroup: 1,
             spaceBetween: 30,
         },
-        768:{
+        1025:{
             slidesPerView: 5.5,
             // slidesPerGroup: 2,
             spaceBetween: 16,
-            centeredSlides: true,
         }
     }
 });
 
+$(function () {
+    $('a[href^="#"]').click(function () {
+        var speed = 500;
+        var href = $(this).attr("href");
+        var target = $(href == "#" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({
+            scrollTop: position
+        }, speed, "swing");
+        return false;
+    });
+
+})
+
+$(function () {
+
+    $('.hamberger__line-wrapper').on('click', function () {
+
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $('.hamberger-menu, #js-menu-bg').removeClass('open');
+
+        } else {
+            $(this).addClass('open');
+            $('.hamberger-menu, #js-menu-bg').addClass('open');
+
+        }
+
+    });
+
+});
+
+$('#hamberger a[href]').on('click', function (event) {
+    $('.hamberger__line-wrapper').trigger('click');
+});
+
+
+$('#js-menu-bg').on('click', function () {
+    if ($(this).hasClass('open')) {
+        $(this).removeClass('open');
+        $('.hamberger-menu, .hamberger__line-wrapper').removeClass('open');
+    }
+});
